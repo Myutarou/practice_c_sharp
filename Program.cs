182eac79e1544b61d5fb692bcff6820bb36734f4
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2024-01-02 21:16:08 +0900

    yamldotnetでymlをロードできるところまで完成

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using YamlDotNet.RepresentationModel;
+using System.Text;
+
+/// <summary>
+/// botを起動する名前空間
+/// </summary>
+namespace Program
+{
+    /// <summary>
+    /// botを起動するMain関数を保持するクラス
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// ymlファイルのURI
+        /// </summary>
+        private static string yamlPath = "data.yml";
+        public static void Main()
+        {
+            // ymlデータのロード
+            var input = new StreamReader(yamlPath, Encoding.UTF8);
+            var yaml = new YamlStream();
+            yaml.Load(input);
+            var rootNode = yaml.Documents[0].RootNode;
+
+            Console.WriteLine(rootNode["api"]);
+        }
+    }
+
+}
