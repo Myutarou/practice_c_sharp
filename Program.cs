7e5a5d86a49f4b82af67d0fc8219541f2fb57ffa
negitoro001 <stnjllx7157@mc.pikara.ne.jp>
2024-01-03 03:45:39 +0900

    ymlデータ部をカプセル化

--- Program.cs
+++ Program.cs
@@ -17,13 +17,27 @@ namespace Program
         private static string yamlPath = "data.yml";
         public static void Main()
         {
+            Console.WriteLine(GetYamlData("api"));
+        }
+
+        /// <summary>
+        /// yamlデータを抽出する関数
+        /// </summary>
+        /// <param name="data">ymlのデータ</param>
+        /// <returns>指定されたデータを返す</returns>
+        public static string GetYamlData(string data)
+        {
             // ymlデータのロード
             var input = new StreamReader(yamlPath, Encoding.UTF8);
             var yaml = new YamlStream();
             yaml.Load(input);
             var rootNode = yaml.Documents[0].RootNode;
 
-            Console.WriteLine(rootNode["api"]);
+#pragma warning disable CS8600 // Null リテラルまたは Null の可能性がある値を Null 非許容型に変換しています。
+#pragma warning disable CS8603 // Null 参照戻り値である可能性があります。
+            return (string)rootNode[data];
+#pragma warning restore CS8603 // Null 参照戻り値である可能性があります。
+#pragma warning restore CS8600 // Null リテラルまたは Null の可能性がある値を Null 非許容型に変換しています。
         }
     }
 
